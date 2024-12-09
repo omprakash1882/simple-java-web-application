34c97a0a6515e54a34b0460af25d8073e4f81e96
om <om@oms-MacBook-Air.local>
2024-12-09 12:26:17 +0530

    my java application

---
+++ src/main/java/com/docker/spring/Application.java
@@ -0,0 +1,34 @@
+// package com.docker.spring;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class Application {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(Application.class, args);
+// 	}
+
+// }
+package com.docker.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Dockerizing Spring Boot Application";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
